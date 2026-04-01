3f7e658cd408a4fd749e490a4e58b9b8a36aaae9
Cmini <3035953865@qq.com>
2026-04-01 20:32:31 +0800

    video

--- cpp/main.cc
+++ cpp/main.cc
@@ -1,9 +1,8 @@
 /*****************************************************************************
- * @copyright Copyright © luominghui Co., Ltd. 2026-2036. All rights reserved.
+ * @copyright Copyright luominghui Co., Ltd. 2026-2036. All rights reserved.
  * @link www.alientek.com / https://space.bilibili.com/9665395
  * @file main.cc
- * @brief 该程序在rv1126b上使用端到端yolo26n模型进行目标检测,
- * 		  并通过 OpenCV 进行摄像头视频流处理并显示结果。
+ * @brief Run YOLO26n on RV1126B and display detection results on a video file.
  * @author luominghui
  * @note 2026-1-16 v1.0:first release
  ****************************************************************************/
@@ -16,6 +15,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <chrono>
+#include <iostream>
 
 #include "yolo26.h"
 #include "image_utils.h"
@@ -24,21 +24,18 @@
 #include <opencv2/opencv.hpp>
 
 /*-------------------------------------------
-                  Main Function
+  Main Function
 -------------------------------------------*/
 int main(int argc, char **argv)
 {
-    if (argc <= 2 || argc>3)
+    if (argc != 3)
     {
-        printf("%s <model_path> [camera_index]\n", argv[0]);
+        printf("%s <model_path> <video_path>\n", argv[0]);
         return -1;
     }
 
     const char *model_path = argv[1];
-    int camera_index = 31; // default value
-    if (argc == 3) {
-        camera_index = atoi(argv[2]);
-    }
+    const char *video_path = argv[2];
 
     int ret;
     rknn_app_context_t rknn_app_ctx;
@@ -50,58 +47,61 @@ int main(int argc, char **argv)
     if (ret != 0)
     {
         printf("init_yolo26_model fail! ret=%d model_path=%s\n", ret, model_path);
+        deinit_post_process();
+        return -1;
+    }
+
+    cv::VideoCapture cap(video_path);
+    if (!cap.isOpened())
+    {
+        std::cerr << "Failed to open the video file: " << video_path << std::endl;
+        release_yolo26_model(&rknn_app_ctx);
+        deinit_post_process();
+        return -1;
     }
 
     image_buffer_t src_image;
     memset(&src_image, 0, sizeof(image_buffer_t));
     object_detect_result_list od_results;
 
-    cv::VideoCapture cap(camera_index);
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    if (!cap.isOpened()) {
-        std::cerr << "Failed to open the camera." << std::endl;
-        return -1;
-    }
-    
     cv::Mat src_frame;
+    cv::Mat rgb_frame;
+    cv::Mat display_frame;
     cv::namedWindow("out", cv::WINDOW_NORMAL);
-    cv::setWindowProperty("out", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
-    
+
     int frame_count = 0;
     auto start_time = std::chrono::steady_clock::now();
     double fps = 0.0;
 
-    double recent_npu_times[10] = {0}; 
-    int npu_time_index = 0; 
-    int npu_time_count = 0; 
-    double avg_npu_time = 0.0; 
-    double npu_fps = 0.0; 
-    
-    while(true){
-        auto frame_start = std::chrono::steady_clock::now();
-        
-        cap >> src_frame;
-        if (src_frame.empty()) {
-            std::cerr << "Failed to grab frame from the camera." << std::endl;
+    double recent_npu_times[10] = {0};
+    int npu_time_index = 0;
+    int npu_time_count = 0;
+    double avg_npu_time = 0.0;
+    double npu_fps = 0.0;
+
+    while (cap.read(src_frame))
+    {
+        if (src_frame.empty())
+        {
+            std::cerr << "Video reached the end or failed to read frame." << std::endl;
             break;
         }
 
-        cv::rotate(src_frame, src_frame, cv::ROTATE_90_CLOCKWISE);//cv::ROTATE_90_COUNTERCLOCKWISE or cv::ROTATE_180
-        src_image.height = src_frame.rows;
-        src_image.width = src_frame.cols;
-        src_image.width_stride = src_frame.step[0];
-        src_image.virt_addr = src_frame.data;
-        src_image.format = IMAGE_FORMAT_RGB888;
-        src_image.size = src_frame.total() * src_frame.elemSize();
+        cv::cvtColor(src_frame, rgb_frame, cv::COLOR_BGR2RGB);
 
-        int ret;
+        src_image.height = rgb_frame.rows;
+        src_image.width = rgb_frame.cols;
+        src_image.width_stride = rgb_frame.step[0];
+        src_image.virt_addr = rgb_frame.data;
+        src_image.format = IMAGE_FORMAT_RGB888;
+        src_image.size = rgb_frame.total() * rgb_frame.elemSize();
+
         image_buffer_t dst_img;
         letterbox_t letter_box;
         rknn_input inputs[rknn_app_ctx.io_num.n_input];
         rknn_output outputs[rknn_app_ctx.io_num.n_output];
-        const float nms_threshold = NMS_THRESH;      // 默认的NMS阈值
-        const float box_conf_threshold = BOX_THRESH; // 默认的置信度阈值
+        const float nms_threshold = NMS_THRESH;
+        const float box_conf_threshold = BOX_THRESH;
         int bg_color = 114;
 
         memset(&od_results, 0x00, sizeof(od_results));
@@ -110,7 +110,6 @@ int main(int argc, char **argv)
         memset(inputs, 0, sizeof(inputs));
         memset(outputs, 0, sizeof(outputs));
 
-        // Pre Process
         dst_img.width = rknn_app_ctx.model_width;
         dst_img.height = rknn_app_ctx.model_height;
         dst_img.format = IMAGE_FORMAT_RGB888;
@@ -119,18 +118,23 @@ int main(int argc, char **argv)
         if (dst_img.virt_addr == NULL)
         {
             printf("malloc buffer size:%d fail!\n", dst_img.size);
+            cap.release();
+            release_yolo26_model(&rknn_app_ctx);
+            deinit_post_process();
             return -1;
         }
 
-        // letterbox
         ret = convert_image_with_letterbox(&src_image, &dst_img, &letter_box, bg_color);
         if (ret < 0)
         {
             printf("convert_image_with_letterbox fail! ret=%d\n", ret);
+            free(dst_img.virt_addr);
+            cap.release();
+            release_yolo26_model(&rknn_app_ctx);
+            deinit_post_process();
             return -1;
         }
 
-        // Set Input Data
         inputs[0].index = 0;
         inputs[0].type = RKNN_TENSOR_UINT8;
         inputs[0].fmt = RKNN_TENSOR_NHWC;
@@ -138,53 +142,60 @@ int main(int argc, char **argv)
         inputs[0].buf = dst_img.virt_addr;
 
         ret = rknn_inputs_set(rknn_app_ctx.rknn_ctx, rknn_app_ctx.io_num.n_input, inputs);
+        free(dst_img.virt_addr);
         if (ret < 0)
         {
             printf("rknn_input_set fail! ret=%d\n", ret);
+            cap.release();
+            release_yolo26_model(&rknn_app_ctx);
+            deinit_post_process();
             return -1;
         }
-        free(dst_img.virt_addr);
-        
 
         auto npu_start = std::chrono::steady_clock::now();
         ret = rknn_run(rknn_app_ctx.rknn_ctx, nullptr);
         if (ret < 0)
         {
             printf("rknn_run fail! ret=%d\n", ret);
+            cap.release();
+            release_yolo26_model(&rknn_app_ctx);
+            deinit_post_process();
             return -1;
         }
         auto npu_end = std::chrono::steady_clock::now();
         double current_npu_time = std::chrono::duration<double, std::milli>(npu_end - npu_start).count();
-        
+
         recent_npu_times[npu_time_index] = current_npu_time;
         npu_time_index = (npu_time_index + 1) % 10;
-        if (npu_time_count < 10) {
+        if (npu_time_count < 10)
+        {
             npu_time_count++;
         }
-        
 
         double total_npu_time = 0.0;
-        for (int i = 0; i < npu_time_count; i++) {
+        for (int i = 0; i < npu_time_count; i++)
+        {
             total_npu_time += recent_npu_times[i];
         }
         avg_npu_time = total_npu_time / npu_time_count;
-        
         npu_fps = avg_npu_time > 0 ? 1000.0 / avg_npu_time : 0.0;
 
-        // Get Output
-        memset(outputs, 0, sizeof(outputs));
         for (int i = 0; i < rknn_app_ctx.io_num.n_output; i++)
         {
             outputs[i].index = i;
             outputs[i].want_float = (!rknn_app_ctx.is_quant);
         }
         ret = rknn_outputs_get(rknn_app_ctx.rknn_ctx, rknn_app_ctx.io_num.n_output, outputs, NULL);
+        if (ret < 0)
+        {
+            printf("rknn_outputs_get fail! ret=%d\n", ret);
+            cap.release();
+            release_yolo26_model(&rknn_app_ctx);
+            deinit_post_process();
+            return -1;
+        }
 
-
-        // Post Process
         post_process(&rknn_app_ctx, outputs, &letter_box, box_conf_threshold, nms_threshold, &od_results);
-
-        // Remeber to release rknn output
         rknn_outputs_release(rknn_app_ctx.rknn_ctx, rknn_app_ctx.io_num.n_output, outputs);
 
         char text[256];
@@ -192,9 +203,10 @@ int main(int argc, char **argv)
         {
             object_detect_result *det_result = &(od_results.results[i]);
             printf("%s @ (%d %d %d %d) %.3f\n", coco_cls_to_name(det_result->cls_id),
-                det_result->box.left, det_result->box.top,
-                det_result->box.right, det_result->box.bottom,
-                det_result->prop);
+                   det_result->box.left, det_result->box.top,
+                   det_result->box.right, det_result->box.bottom,
+                   det_result->prop);
+
             int x1 = det_result->box.left;
             int y1 = det_result->box.top;
             int x2 = det_result->box.right;
@@ -207,43 +219,42 @@ int main(int argc, char **argv)
         }
 
         frame_count++;
-        auto frame_end = std::chrono::steady_clock::now();
-        double frame_time = std::chrono::duration<double, std::milli>(frame_end - frame_start).count();
-        
-        if (frame_count % 10 == 0) { // 每10帧更新一次FPS显示
+        if (frame_count % 10 == 0)
+        {
             auto current_time = std::chrono::steady_clock::now();
             double elapsed_time = std::chrono::duration<double>(current_time - start_time).count();
             fps = frame_count / elapsed_time;
         }
-        
+
         char fps_text[128];
-        sprintf(fps_text, "camera FPS: %.1f\nNPU fps: %.1f", fps, npu_fps);
+        sprintf(fps_text, "video FPS: %.1f\nNPU fps: %.1f", fps, npu_fps);
         draw_text(&src_image, fps_text, 10, 30, COLOR_YELLOW, 10);
-        
-        cv::Mat result_mat = cv::Mat(src_image.height, src_image.width, CV_8UC3, src_image.virt_addr, src_image.width_stride);
-        cv::imshow("out", result_mat);
+
+        cv::Mat result_mat(src_image.height, src_image.width, CV_8UC3, src_image.virt_addr, src_image.width_stride);
+        cv::cvtColor(result_mat, display_frame, cv::COLOR_RGB2BGR);
+        cv::imshow("out", display_frame);
+
         int key = cv::waitKey(1);
-        if (key == 'q' || key == 27) {
+        if (key == 'q' || key == 27)
+        {
             break;
         }
     }
-    
+
     auto end_time = std::chrono::steady_clock::now();
     double total_elapsed_time = std::chrono::duration<double>(end_time - start_time).count();
-    double avg_fps = frame_count / total_elapsed_time;
+    double avg_fps = total_elapsed_time > 0 ? frame_count / total_elapsed_time : 0.0;
     printf("Average FPS: %.2f over %d frames (%.2f seconds)\n", avg_fps, frame_count, total_elapsed_time);
-    
+
+    cap.release();
+    cv::destroyAllWindows();
+
     deinit_post_process();
 
     ret = release_yolo26_model(&rknn_app_ctx);
     if (ret != 0)
     {
         printf("release_yolo26_model fail! ret=%d\n", ret);
-    }
-
-    if (src_image.virt_addr != NULL)
-    {
-        free(src_image.virt_addr);
     }
 
     return 0;
